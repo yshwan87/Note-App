9a9e101d1fbfae161242c411a8ccce78a5524fe5
yshwan87 <77893199+yshwan87@users.noreply.github.com>
2023-03-07 17:16:53 -0800

    Add the sliver for app bar and have search working

--- my_note_app/lib/screens/home_screen.dart
+++ my_note_app/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,6 +16,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool isDescending = false;
   String dateOrderMenu = "Date Descending";
+  String searchWord = "";
 
   void changeOrder() {
     setState(() {
@@ -22,21 +25,32 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void changeSearchWord(String word) {
+    setState(() {
+      searchWord = word.toLowerCase();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0.0,
-          title: Text('Fire Notes',
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            title: Text(
+              'Note Apps',
               style: GoogleFonts.roboto(
-                color: Colors.black,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 22,
-              )),
-          backgroundColor: Colors.white,
-          actions: [
-            PopupMenuButton(
+              ),
+            ),
+            actions: [
+              PopupMenuButton(
                 icon: const Icon(
                   Icons.menu_sharp,
                   color: Colors.black,
@@ -65,66 +79,107 @@ class _HomeScreenState extends State<HomeScreen> {
                   } else if (value == 2) {
                     print("Logout menu is selected.");
                   }
-                }),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 5.0,
+                },
               ),
-              Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance.collection("Notes").orderBy("creation_date", descending: isDescending).snapshots(),
-                  builder: ((context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (snapshot.hasData) {
-                      return GridView(
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                        ),
-                        children: snapshot.data!.docs
-                            .map(
-                              (note) => noteCard(() {
+            ],
+            bottom: AppBar(
+              title: Container(
+                width: double.infinity,
+                height: 40,
+                color: Colors.white,
+                child: Center(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Search for something',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                    onChanged: (value) => changeSearchWord(value),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance.collection("Notes").orderBy("creation_date", descending: isDescending).snapshots(),
+                    builder: ((context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (snapshot.hasData) {
+                        var data = snapshot.data!.docs;
+                        return GridView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            var note = data[index];
+                            if (searchWord == "") {
+                              return noteCard(() {
                                 Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: ((context) => NoteEditorScreen(note)),
+                                  ),
+                                );
+                              }, note);
+                            } else {
+                              var isContainSearchWord = data[index]["note_content"].toString().toLowerCase().contains(searchWord);
+                              if (isContainSearchWord) {
+                                return noteCard(() {
+                                  Navigator.push(
                                     context,
                                     MaterialPageRoute(
                                       builder: ((context) => NoteEditorScreen(note)),
-                                    ));
-                              }, note),
-                            )
-                            .toList(),
+                                    ),
+                                  );
+                                }, note);
+                              } else {
+                                return const SizedBox(
+                                  width: 0.0,
+                                  height: 0.0,
+                                );
+                              }
+                            }
+                          },
+                          padding: const EdgeInsets.only(top: 0.0),
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          shrinkWrap: true,
+                          scrollDirection: Axis.vertical,
+                        );
+                      }
+                      return Text(
+                        "There is no Note",
+                        style: GoogleFonts.nunito(color: Colors.white),
                       );
-                    }
-                    return Text(
-                      "There is no Note",
-                      style: GoogleFonts.nunito(color: Colors.white),
-                    );
-                  }),
+                    }),
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: ((context) => const NoteEditorScreen(null)),
-              ),
-            );
-          },
-          label: const Text("Add Note"),
-          icon: const Icon(Icons.add),
-        ));
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: ((context) => const NoteEditorScreen(null)),
+            ),
+          );
+        },
+        label: const Text("Add Note"),
+        icon: const Icon(Icons.add),
+      ),
+    );
   }
 }
